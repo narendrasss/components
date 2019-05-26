46f4a9664f4edc42342de58bc5e282db273e82a9
Narendras <n.syahrasyad.n@gmail.com>
2019-05-26 10:57:32 -0700

    Updated input component to allow external refs

--- src/Input/index.js
+++ src/Input/index.js
@@ -2,18 +2,29 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useRef } from 'react'
 import { string, node } from 'prop-types'
+import { refPropType } from '../utils'
 import clsx from 'clsx'
 import './style.scss'
 
 function Input(props) {
   const inputEl = useRef(null)
-  const { className, before, after, prefix, suffix, ...inputProps } = props
+  const { className, before, after, prefix, suffix, inputRef, ...inputProps } = props
+
+  const attachRef = el => {
+    inputEl.current = el
+    if (typeof inputRef === 'function') {
+      inputRef(el)
+    } else {
+      inputRef.current = el
+    }
+  }
+
   return (
     <div className={clsx('input__wrapper', className)}>
       {before && <div className="input__container input__prefix">{before}</div>}
       <div className="input__container" onClick={() => inputEl.current.focus()}>
         {prefix && <span className="input__icon">{prefix}</span>}
-        <input className="input" {...inputProps} ref={inputEl} />
+        <input className="input" {...inputProps} ref={attachRef} />
         {suffix && <span className="input__icon">{suffix}</span>}
       </div>
       {after && <div className="input__container input__prefix">{after}</div>}
@@ -26,7 +37,8 @@ Input.propTypes = {
   before: node,
   after: node,
   prefix: node,
-  suffix: node
+  suffix: node,
+  inputRef: refPropType
 }
 
 export default Input
--- src/utils/propTypes.js
+++ src/utils/propTypes.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 export const refPropType = PropTypes.oneOfType([
   PropTypes.func,
-  PropTypes.shape({ current: PropTypes.instanceOf(Element) })
+  PropTypes.shape({ current: PropTypes.element })
 ])
 
 export default { refPropType }
